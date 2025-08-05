447beacb1a7ca37c1bd955dc7ab66e49fa06aaa1
Naoya Kohda <naoya.kohda@gmail.com>
2025-08-06 05:48:18 +0900

    React.useMemo内でのmissingAddressesの定義を移動し、依存配列から削除

--- frontend/src/components/NodeListDisplayContainer.tsx
+++ frontend/src/components/NodeListDisplayContainer.tsx
@@ -17,13 +17,13 @@ const NodeListDisplayContainer: React.FC = () => {
     const universeNumbers: ArtNet.Universe[] = Object.keys(universes).map(Number) as ArtNet.Universe[]
     receiveUniverseByNode.set(address, universeNumbers)
   }
-  // ノードが存在しないがdmxDataにだけ存在するアドレス
-  const missingAddresses = Array.from(receiveUniverseByNode.keys()).filter(
-    address => !artNetNodes.some(node => node.IPAddress === address)
-  )
 
-  const displayNodes: NodeListDisplayNode[] = React.useMemo(
-    () => [
+  const displayNodes: NodeListDisplayNode[] = React.useMemo(() => {
+    // ノードが存在しないがdmxDataにだけ存在するアドレス
+    const missingAddresses = Array.from(receiveUniverseByNode.keys()).filter(
+      address => !artNetNodes.some(node => node.IPAddress === address)
+    )
+    return [
       ...artNetNodes.map(node => ({
         address: node.IPAddress,
         info: node,
@@ -36,9 +36,8 @@ const NodeListDisplayContainer: React.FC = () => {
         universes: receiveUniverseByNode.get(address) || [],
         isUnknown: true,
       })),
-    ],
-    [artNetNodes, receiveUniverseByNode, missingAddresses]
-  )
+    ]
+  }, [artNetNodes, receiveUniverseByNode])
 
   return <NodeListDisplay nodes={displayNodes} />
 }
