ef8eb19c30c33afac1cb913433e5d64f4fbe0fd6
Naoya Kohda <naoya.kohda@gmail.com>
2025-08-05 23:31:27 +0900

    ResizeObserverのモックを追加

--- frontend/test-setup.ts
+++ frontend/test-setup.ts
@@ -2,3 +2,11 @@ import { vi } from 'vitest'
 
 // fetch APIのモック
 global.fetch = vi.fn() as unknown as typeof fetch
+
+// ResizeObserver のモック
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+global.ResizeObserver = ResizeObserver
